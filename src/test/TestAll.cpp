a74a78faf0918e49481f66f7242ee5121d30b314
Martin Gerhardy <martin.gerhardy@gmail.com>
2014-12-04 21:10:41 +0100

    * moved into subdir and started test

---
+++ src/test/TestAll.cpp
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <gtest/gtest.h>
+#include <iostream>
+
+class LocalEnv: public ::testing::Environment {
+public:
+	virtual ~LocalEnv() {
+	}
+	virtual void SetUp() override {
+	}
+	virtual void TearDown() override {
+	}
+};
+
+extern "C" int main (int argc, char **argv)
+{
+	::testing::AddGlobalTestEnvironment(new LocalEnv);
+	//::testing::GTEST_FLAG(throw_on_failure) = true;
+	::testing::InitGoogleTest(&argc, argv);
+
+	try {
+		return RUN_ALL_TESTS();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+}
